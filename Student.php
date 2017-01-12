58f07967476d5e372f996d099cdaeaa27a855079
Chantal <Chantal@192.168.1.69>
2017-01-11 20:52:23 -0800

    Setup

--- Student.php
+++ Student.php
@@ -16,9 +16,22 @@
 class Student {
     //put your code here
     function __construct() {
-    $this->surname = '';
-    $this->first_name = '';
-    $this->emails = array();
-    $this->grades = array();
-}
+        $this->surname = '';
+        $this->first_name = '';
+        $this->emails = array();
+        $this->grades = array();
+    }
+    function add_email($which,$address) {
+        $this->emails[$which] = $address;
+    }
+
+    function add_grade($grade) {
+        $this->grades[] = $grade;
+    }
+    function average() {
+        $total = 0;
+        foreach ($this->grades as $value)
+            $total += $value;
+        return $total / count($this->grades);
+    }
 }
